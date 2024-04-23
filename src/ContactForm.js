9187aab9e780f94f814fcc47c8cf80e6f0b2c928
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2024-04-22 23:10:21 -0600

    Building email var

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ContactForm = () => {
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [stateMessage, setStateMessage] = useState(null)
+
+    const sendEmail = (e) => {
+        e.persist()
+        e.preventDefault()
+        setIsSubmitting(true)
+    }
   return (
     <div>ContactForm</div>
   )
