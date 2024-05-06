61fcf5c693cf2aaca773be0ad1bbf79859e9dc2a
Redsnow125 <128419959+Redsnow125@users.noreply.github.com>
2024-05-06 00:29:59 -0600

    Bug Fixing

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -1,48 +1,35 @@
-import React, { useState } from 'react'
-import emailjs from '@emailjs/browser'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-const ContactForm = () => {
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [stateMessage, setStateMessage] = useState(null);
+export const ContactForm = () => {
+  const form = useRef();
+
   const sendEmail = (e) => {
-    e.persist();
     e.preventDefault();
-    setIsSubmitting(true);
+
     emailjs
-      .sendForm(
-        process.env.REACT_APP_SERVICE_ID,
-        process.env.REACT_APP_TEMPLATE_ID,
-        e.target,
-        process.env.REACT_APP_PUBLIC_KEY
-      )
+      .sendForm('service_t8u3abw', 'template_2432jqh', form.current, {
+        publicKey: 'vF4FPdCj9RMXUNrFi',
+      })
       .then(
-        (result) => {
-          setStateMessage('Message sent!');
-          setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage(null);
-          }, 5000);
+        () => {
+          console.log('SUCCESS!');
         },
         (error) => {
-          setStateMessage('Something went wrong, please try again later');
-          setIsSubmitting(false);
-          setTimeout(() => {
-            setStateMessage(null);
-          }, 5000); 
-        }
+          console.log('FAILED...', error.text);
+        },
       );
-        e.target.reset();
   };
+
   return (
-    <form onSubmit={sendEmail}>
-      <label>Enter Name</label>
+    <form ref={form} onSubmit={sendEmail}>
+      <label>Name</label>
       <input type="text" name="user_name" />
       <label>Email</label>
       <input type="email" name="user_email" />
       <label>Message</label>
       <textarea name="message" />
-      <input type="submit" value="Send" disabled={isSubmitting} />
-      {stateMessage && <p>{stateMessage}</p>}
+      <input type="submit" value="Send" />
     </form>
   );
 };
